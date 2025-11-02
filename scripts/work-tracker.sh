9cc600c7e42aedc68139e121cbf71ebc0b624fd9
Tumbleweed <tuumbleweed@users.noreply.github.com>
2025-11-02 11:36:08 -0500

    scripts: Add work-tracker.sh

---
+++ scripts/work-tracker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eo pipefail # Exit immediately if any command returns a non-zero status
+
+# source the shared functions
+source "$(dirname "${BASH_SOURCE[0]}")/shared.sh"
+
+cd_to_project_dir
+source ./env/.env.sh
+./build/tracker
